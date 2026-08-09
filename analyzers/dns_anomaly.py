4f6861e5c7fbfbc957677840b55135a3c253430f
HunterBFranklin <hunterfranklinb@gmail.com>
2026-08-09 10:00:49 -0700

    feat: Completed the implementation for extract_dns_queries(). Will be working on getting the TLD function squared away.

--- analyzers/dns_anomaly.py
+++ analyzers/dns_anomaly.py
@@ -2,5 +2,75 @@
 # pcap-analyzer — dns_anomaly.py
 # GitHub Repo : github.com/HunterBFranklin/pcap-analyzer
 # Created     : Aug. 7, 2026
-# Modified    : Aug. 7, 2026
+# Modified    : Aug. 8, 2026
 # =============================================================================
+
+from scapy.all import *
+from math import log2
+from collections import Counter
+
+def extract_dns_queries(packets):
+
+    """
+    Extracts all Domain Name System (DNS) queries out of a raw packet list
+    and returns them in a cleaner form. Forms a Python object, rather than having
+    raw Scapy data.
+    """
+
+    queries = []
+
+    for packet in packets:
+        if packet.haslayer(DNS) and packet.haslayer(DNSQR):
+            queried_domain = packet[DNSQR].qname.decode().rstrip('.')
+            if packet.haslayer(IP):
+                src_ip = packet[IP].src
+                queries.append({
+                    'src_ip': src_ip, 
+                    'queried_domain': queried_domain})
+
+    return queries
+
+
+def get_tld(domain: str):
+
+    """
+    Extracts the Top Level Domain (TLD) from a domain string and returns it.
+    """
+
+    pass
+
+
+def get_sld(domain: str):
+
+    """
+    
+    """
+
+    pass
+
+
+def shannon_entropy(s: str):
+
+    """
+    
+    """
+
+    pass
+
+
+def is_rare_tld(tld: str, blocklist: set):
+
+    """
+    
+    """
+
+    pass
+
+
+def analyze_dns(packets, tld_blocklist: set, entropy_threshold: float):
+
+    """
+    
+    """
+
+    pass
--- feeds/DEVLOG.md
+++ feeds/DEVLOG.md
@@ -1,6 +1,8 @@
 # Development Log (most recent first)
 
-My goal with this devlog it to show full authorship in the AI age of coding.
+My goal with this devlog is to show full authorship of this project in the AI age of coding.
+
+**Aug. 9, 2026 - 1:** I began working on dns_anomaly.py. I used Scapy's haslayer() for guarding the queries to ensure they are within the right layer to be seen and to ensure the packet holds the necessary information for extraction. The purpose of this function is to extract all of the DNS queries from a raw packet and clean it up to be used it checking for DNS anomalies like DoH use, a DGA, or a random DNS resolver.
 
 **Aug. 8, 2026 - 2:** I completed the beaconing.py implementation. I got pretty confused on how I wanted to structure the alert and what information was important for later files. The output works. I grabbed a sample malware packet to show a positive flag within the cv_threshold that I set (to 0.2) from malware-traffic-analysis.net. In later additions, I plan to let the user change the cv_threshold in CLI, making it adjustable rather than hardcoded. I was able to make a simple test using the sample pcap file and it worked; showing a single detection that was flagged as high severity and suspicious. I next plan to work on dns_anomaly.py.
 
